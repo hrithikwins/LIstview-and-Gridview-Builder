6849a8bda3dd796ae8feedfba31e18040034f650
hrithikrtiwari <hrithikrtiwari@gmail.com>
2021-03-27 04:08:29 +0530

    comma for formatting in list.dart

--- newapp/lib/list.dart
+++ newapp/lib/list.dart
@@ -6,25 +6,20 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       title: 'Material App',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Material App Bar'),
-          ),
-          body: ListView.builder(
-              itemCount: 2,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text('one'),
-                
-                  
-                );
-              }
-              )
-              
-              ),
-              
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: ListView.builder(
+          itemCount: 2,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text('one'),
+            );
+          },
+        ),
+      ),
     );
   }
 }
