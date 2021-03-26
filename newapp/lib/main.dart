dcc5e3a58dc095ee7e171fa59df46a4217fdb493
hrithikrtiwari <hrithikrtiwari@gmail.com>
2021-03-27 04:07:44 +0530

    added named routes

--- newapp/lib/main.dart
+++ newapp/lib/main.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:newapp/list.dart';
 
- 
 void main() => runApp(MaterialApp(
-  title:'MyApp',
-  home: MyApp(),
-  theme: ThemeData(primaryColor: Colors.amber),
-));
- 
+      title: 'MyApp',
+      // implementing named routes in our material app
+      // home: MyApp(), The home parameter is now no longer needed and now initialroute parameter is used instead
+      initialRoute: '/home', // the /home is defined in `routes`
+      // The Routes takes in a list shown below
+      routes: {
+        "/home": (context) => MyApp(),
+        "/second": (context) => SecondPage(),
+      },
+      theme: ThemeData(primaryColor: Colors.amber),
+    ));
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.teal),
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: Text('Material App Bar'),
-        ),
-        body: GridView.builder(gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2)
-        , itemBuilder: (BuildContext context,int index){
-          return ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondPage()));
-          },child: Text('one'),
-          
-          );
-        })
+      home: SafeArea(
+        child: Scaffold(
+            // removed appbar and wrapped scaffold with safeArea
+            body: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (BuildContext context, int index) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      // changing .push to .pushnamed
+                      Navigator.pushNamed(context, '/second');
+                    },
+                    child: Text('one'),
+                  );
+                })),
       ),
     );
   }
