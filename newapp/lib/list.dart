f38e13b2163e6a2e8ebd825492351548ff521d2c
hrithikrtiwari <hrithikrtiwari@gmail.com>
2021-03-27 05:35:20 +0530

    MaterialApp to Scaffold

--- newapp/lib/list.dart
+++ newapp/lib/list.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
-
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: ListView.builder(
-          itemCount: 2,
+    return Scaffold(
+      body: Container(
+        child: ListView.builder(
+          itemCount: 200,
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
               title: Text('one'),
+              onTap: () => Navigator.pushNamed(context, '/'),
             );
           },
         ),
